99ed1643cd21c913b8cf01fd685ca775c482a4d8
Dego <48290509+anishkandel@users.noreply.github.com>
2025-03-23 18:15:57 +0545

    Update settings.py

--- NepaFund/crowdnepal/settings.py
+++ NepaFund/crowdnepal/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '^woj)$_6*k25-dg=h2c6-$yld@5xkzom2v94q#tkst+m910+o4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS =  ['*']
 
 # Application definition
 
